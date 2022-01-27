79e7395a307afef849fc4835187108884e110c6b
Stanley Pan <imoment.developer@gmail.com>
2022-01-27 15:13:24 -0500

    Initial Commit

---
+++ Carousel_Animation/Carousel_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carousel_AnimationApp.swift
+//  Carousel_Animation
+//
+//  Created by Stanley Pan on 2022/01/27.
+//
+
+import SwiftUI
+
+@main
+struct Carousel_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
